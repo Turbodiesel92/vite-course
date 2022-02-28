7ace897f799d3eb4f2d29d6b1889deed82d35fc0
Turbodiesel92 <sirmatthew@DESKTOP-E2O47QT.localdomain>
2022-02-28 15:52:29 -0600

    vue-course1

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from "@/components/HelloWorld.vue";
+import styles from "./assets/example.module.css";
+const developerMood: string = "Thrilled for Typescript!";
+console.log(developerMood);
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/tiny-vue-logo.png" />
+  <h1 :class="styles.example">Hello CSS Modules</h1>
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/main.css";
+
+const autoImportedModules = import.meta.globEager("./autoImports/*.js");
+
+
+createApp(App).mount("#app");
